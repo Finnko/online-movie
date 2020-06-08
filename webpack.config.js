ffc381e61fdfa85b3af5fb819d9fe2cdcf81bece
Alexey Khabirov <finko85@gmail.com>
2020-06-08 23:00:50 +0300

    installs deps

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require(`path`);
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
+
+module.exports = (env) => {
+    const isDev = env.mode === 'development';
+    const isProd = !isDev;
+
+    return {
+        entry: `./src/index.js`,
+        mode: isProd ? 'production' : isDev && 'development',
+        output: {
+            filename: `bundle.js`,
+            path: path.join(__dirname, `public`)
+        },
+        devServer: {
+            contentBase: path.join(__dirname, `public`),
+            publicPath: `/`,
+            // proxy: {
+            //     '/offer': {
+            //         target: `http://localhost:1337/`,
+            //         pathRewrite: {'^/offer': ``},
+            //     },
+            // },
+            open: true,
+            inline: true,
+            port: 1337,
+            historyApiFallback: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: `babel-loader`,
+                    },
+                }
+            ],
+        },
+        devtool: isDev ? `eval-source-map` : `none`,
+        plugins: [
+            new MomentLocalesPlugin({
+                localesToKeep: [`en`],
+            }),
+        ],
+    }
+};
